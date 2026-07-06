50e6031959e4db8a5cd52ce7cf6d25facecb0519
Shivanshu Mangal <shivanshumangal007@gmail.com>
2026-07-06 13:27:15 +0530

    Refactor handlers to use application struct and add error handling methods

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -22,36 +21,37 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		http.Error(w, "failed to load templete of the file"+err.Error(), http.StatusBadRequest)
+		app.errorLog.Print(err.Error())
+		app.servererror(w, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.errorLog.Print(err.Error())
+		app.servererror(w, err)
 		return
 	}
 	// w.Write([]byte("hello from home route \n"))
 }
-func Snippetview(w http.ResponseWriter, r *http.Request) {
+func (app *application) Snippetview(w http.ResponseWriter, r *http.Request) {
 
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil {
-		http.Error(w, "invalid id: "+err.Error(), http.StatusBadRequest)
+		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 	if id < 1 {
-		http.Error(w, "id should be greater than 1", http.StatusBadRequest)
+		app.notFound(w)
 		return
 	}
 	fmt.Fprintf(w, "view your snippet view here \n id => %d", id)
 }
-func Snippetcreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) Snippetcreate(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
 		w.Header().Set("allow", "POST")
-		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("cerate your new snippet here \n"))
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,11 @@ import (
 	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	fmt.Println("hello world how are you man i am good")
 	addr := flag.String("addr", ":4000", "addres of the the host")
@@ -16,19 +21,21 @@ func main() {
 	infolog := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
 	errLog := log.New(os.Stderr, "ERROR: \t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	mux := http.NewServeMux()
-
 	fileserver := http.FileServer(http.Dir("./ui/static"))
+	mux := http.NewServeMux()
 
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errLog,
 		Handler:  mux,
 	}
-
-	mux.HandleFunc("/", Home)
-	mux.HandleFunc("/snippet/view", Snippetview)
-	mux.HandleFunc("/snippet/create", Snippetcreate)
+	app := &application{
+		errorLog: errLog,
+		infoLog:  infolog,
+	}
+	mux.HandleFunc("/", app.Home)
+	mux.HandleFunc("/snippet/view", app.Snippetview)
+	mux.HandleFunc("/snippet/create", app.Snippetcreate)
 	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
 
 	infolog.Printf("Starting server on %s", *addr)
